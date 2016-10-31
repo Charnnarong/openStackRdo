7cbc83b7745f7e59bb4b8288b8888b138b3bf813
Charnnarong Chomthiang <charnnarongchomthian@nbnco.com.au>
2016-10-31 16:56:31 +0700

    Change Ansible from community to use yum repos.

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -5,6 +5,7 @@ ansible --version
 if [ $? == 0 ]; then
     echo "Successfully install Ansible"
 else
+	
     sudo yum -y group install "Development Tools"
     sudo yum -y install \
                     git \
@@ -34,20 +35,21 @@ else
                     sshpass.x86_64
     sudo pip install --upgrade pip
     sudo pip install paramiko PyYAML Jinja2 httplib2 six pexpect
-    if [ ! -d /vagrant/ansible_community ]; then
-        sudo rm -rf /vagrant/ansible_community
-    fi
-    sudo mkdir /vagrant/ansible_community/
-    sudo git clone git://github.com/ansible/ansible.git --recursive /vagrant/ansible_community/
-    source /vagrant/ansible_community/hacking/env-setup
-    cd /vagrant/ansible_community/
-    sudo python setup.py build
-    sudo python setup.py install
-    if [ ! -d /etc/ansible/ ]; then
-        sudo rm -rf /etc/ansible
-    fi
-    sudo mkdir /etc/ansible/
+#     if [ ! -d /vagrant/ansible_community ]; then
+#         sudo rm -rf /vagrant/ansible_community
+#     fi
+#     sudo mkdir /vagrant/ansible_community/
+#     sudo git clone git://github.com/ansible/ansible.git --recursive /vagrant/ansible_community/
+#     source /vagrant/ansible_community/hacking/env-setup
+#     cd /vagrant/ansible_community/
+#     sudo python setup.py build
+#     sudo python setup.py install
+#     if [ ! -d /etc/ansible/ ]; then
+#         sudo rm -rf /etc/ansible
+#     fi
+#     sudo mkdir /etc/ansible/
 
+	sudo yum -y install ansible
     sudo cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
 
 fi
